99739e77f185190a634adc47d8ef26853da1561e
David McGrann <dpmcgrann@gmail.com>
2018-08-07 00:43:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/:name' do
+    "My name is #{params['name']}."
+  end
+  
+  get '/:hometown' do |n|
+    "My hometown is #{hometown}."
+  end
+  
+  get '/:favorite_song' do
+    "My favorite song is #{params['favorite_song']}."
+  end
+  
 end
